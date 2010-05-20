d50bf313ccfcd20ec5261967f943a2473e2345f2
fabs.pim <fabs.pim@82765aed-657b-57ca-70e0-02808e051eca>
2010-05-20 20:07:32 +0000

    added GPL License Information

--- trunk/test.php
+++ trunk/test.php
@@ -20,7 +20,7 @@ echo "<br />";
 
 echo "http://www.youtube.com/watch?v=EssrtKyCKVc<br /><br />";
 				
-print_r($oembed->getHTML("http://www.youtube.com/watch?v=EssrtKyCKVc", array('width' => 167, 'height' => 567)));
+print_r($oembed->getHTML("http://www.youtube.com/watch?v=EssrtKyCKVc", array('width' => 300)));
 
 echo "<br /><br /><br />";
 echo "-----------------------------------------------------------------------------------<br />";
@@ -28,7 +28,7 @@ echo "<br />";
 echo "<br />";
 echo "http://www.flickr.com/photos/scheiss_benji/4615831545/<br /><br />";
 				
-print_r($oembed->getHTML("http://www.flickr.com/photos/scheiss_benji/4615831545/", array('width' => 167, 'height' => 567)));
+print_r($oembed->getHTML("http://www.flickr.com/photos/scheiss_benji/4615831545/", array('width' => 300)));
 
 echo "<br /><br /><br />";
 echo "-----------------------------------------------------------------------------------<br />";
@@ -36,7 +36,7 @@ echo "<br />";
 echo "<br />";
 echo "http://www.flickr.com/photos/nattu/3094752363/<br /><br />";
 				
-print_r($oembed->getHTML("http://www.flickr.com/photos/nattu/3094752363/", array('width' => 167, 'height' => 567)));	
+print_r($oembed->getHTML("http://www.flickr.com/photos/nattu/3094752363/", array('width' => 300)));	
 
 echo "<br /><br /><br />";
 echo "-----------------------------------------------------------------------------------<br />";
@@ -44,7 +44,7 @@ echo "<br />";
 echo "<br />";
 echo "http://vimeo.com/11774747<br /><br />";
 				
-print_r($oembed->getHTML("http://vimeo.com/11774747", array('width' => 167, 'height' => 567)));
+print_r($oembed->getHTML("http://vimeo.com/11774747", array('width' => 300)));
 
 echo "<br /><br /><br />";
 echo "-----------------------------------------------------------------------------------<br />";
@@ -52,7 +52,7 @@ echo "<br />";
 echo "<br />";
 echo "http://www.viddler.com/explore/sidequesting/videos/23/<br /><br />";
 				
-print_r($oembed->getHTML("http://www.viddler.com/explore/sidequesting/videos/23/", array('width' => 167, 'height' => 567)));
+print_r($oembed->getHTML("http://www.viddler.com/explore/sidequesting/videos/23/", array('width' => 300)));
 
 
 
